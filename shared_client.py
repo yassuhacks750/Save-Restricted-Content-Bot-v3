cacbc83505eba10e520fce0e2477ce0b7f580345
yassuhacks750 <yassudj8085231272@gmail.com>
2025-06-15 17:06:04 +0530

    Update shared_client.py

--- shared_client.py
+++ shared_client.py
@@ -1,28 +1,16 @@
-# Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
-# Licensed under the GNU General Public License v3.0.  
-# See LICENSE file in the repository root for full license text.
-
 from telethon import TelegramClient
-from config import API_ID, API_HASH, BOT_TOKEN, STRING
-from pyrogram import Client
-import sys
+from telethon.errors import FloodWaitError
+import asyncio
 
-client = TelegramClient("telethonbot", API_ID, API_HASH)
-app = Client("pyrogrambot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-userbot = Client("4gbbot", api_id=API_ID, api_hash=API_HASH, session_string=STRING)
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
 
-async def start_client():
-    if not client.is_connected():
-        await client.start(bot_token=BOT_TOKEN)
-        print("SpyLib started...")
-    if STRING:
-        try:
-            await userbot.start()
-            print("Userbot started...")
-        except Exception as e:
-            print(f"Hey honey!! check your premium string session, it may be invalid of expire {e}")
-            sys.exit(1)
-    await app.start()
-    print("Pyro App Started...")
-    return client, app, userbot
-
+async def start_client(bot_token):
+    client = TelegramClient("bot", api_id, api_hash)
+    try:
+        await client.start(bot_token=bot_token)
+    except FloodWaitError as e:
+        print(f"[Telethon] Flood wait: {e.seconds} seconds. Waiting before retry...")
+        await asyncio.sleep(e.seconds)
+        await client.start(bot_token=bot_token)
+    return client
