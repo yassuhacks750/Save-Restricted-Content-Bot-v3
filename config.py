97d7f55cc077914dfd3020c7d360f2b7c4bde3ca
yassuhacks750 <yassudj8085231272@gmail.com>
2025-06-15 16:27:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-# Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
-# Licensed under the GNU General Public License v3.0.  
-# See LICENSE file in the repository root for full license text.
+# Copyright (c) 2025 devgagan : https://github.com/devgaganin.
+# Licensed under the GNU General Public License v3.0.
 
 import os
 from dotenv import load_dotenv
@@ -8,30 +7,28 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # VPS --- FILL COOKIES 🍪 in """ ... """ 
-
 INST_COOKIES = """
-# wtite up here insta cookies
+# write up here insta cookies
 """
 
 YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = os.getenv("22849789", "")
-API_HASH = os.getenv("0fc127c6055acd59f00ec6c229e1e3c4", "")
-BOT_TOKEN = os.getenv("8006760090:AAFGwniB9kz9al10JcnepSg5sQzm3CKHYPQ", "")
+API_ID = 22849789
+API_HASH = "0fc127c6055acd59f00ec6c229e1e3c4"
+BOT_TOKEN = "8006760090:AAFGwniB9kz9al10JcnepSg5sQzm3CKHYPQ"
 MONGO_DB = os.getenv("MONGO_DB", "")
-OWNER_ID = list(map(int, os.getenv("1144684909", "").split())) # list seperated via space
-DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
-STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1144684909")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "-1144684909")) # optional with -100
-MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
-IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
+OWNER_ID = [1144684909]  # आप चाहें तो एक से ज्यादा ID space देकर डाल सकते हैं
+DB_NAME = "telegram_downloader"
+STRING = os.getenv("STRING", None)
+LOG_GROUP = -1144684909
+FORCE_SUB = -1144684909
+MASTER_KEY = "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq"
+IV_KEY = "s7Yx5CpVmE3F"
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
 INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
-FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
-JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro") # this link for start command message
-ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
-
+FREEMIUM_LIMIT = 0
+PREMIUM_LIMIT = 500
+JOIN_LINK = "https://t.me/team_spypro"
+ADMIN_CONTACT = "https://t.me/username_of_admin"
